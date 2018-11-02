c86a209a43dbfba5e4171f2beaaeabb00ce772c3
EvanGee <evangiese77@gmail.com>
2018-11-02 08:09:35 -0600

    Update Website

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/PageLayout/PageLayout'
 import Home from './Home'
-import Events from './Events'
-import ChairTalk from "./ChairTalk"
-import CardEvents from "./CardEvents"
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -13,10 +10,6 @@ export const createRoutes = (store) => ({
   component   : CoreLayout,
   indexRoute  : Home(store),
   childRoutes : [
-    //Events(store),
-    ChairTalk(store),
-    CardEvents(store)
-
   ]
 })
 
